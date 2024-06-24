b72f07ac25656736f6990e6846d873216aee41a9
boki <lsb530@naver.com>
2024-06-25 01:00:20 +0900

    change spring and java version to sync course

---
+++ src/main/java/com/boki/bokispringbatch/BokiSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.boki.bokispringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokiSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BokiSpringBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boki/bokispringbatch/BokiSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boki.bokispringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokiSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
