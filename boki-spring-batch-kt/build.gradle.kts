09e5fd8d9ea86f468d1bcfe57d4d9f365fb03606
boki <lsb530@naver.com>
2024-08-28 02:44:41 +0900

    add h2 to kt module

--- boki-spring-batch-kt/build.gradle.kts
+++ boki-spring-batch-kt/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.batch:spring-batch-test")
+    runtimeOnly("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
